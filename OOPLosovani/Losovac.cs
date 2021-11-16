7d9495e5c733bf530407cb7f8dd12cda0082e8f2
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-11-16 10:22:04 +0100

    Add project files.

---
+++ OOPLosovani/Losovac.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLosovani
+{
+    class Losovac
+    {
+        private List<int> cisla;
+        private Random random;
+
+        public Losovac()
+        {
+            random = new Random();
+            cisla = new List<int>();
+        }
+        public int Losuj()
+        {
+            int cislo = random.Next(100) + 1;
+            cisla.Add(cislo);
+            return cislo;
+
+        }
+        public string Vypis()
+        {
+            string s = "";
+            cisla.Sort();
+            foreach (int i in cisla)
+            {
+                s += i + " ";
+            }
+            return s;
+        }
+
+    }
+}
---
+++ OOPLosovani/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOPLosovani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Losovac losovac = new Losovac();
+            Console.WriteLine("Vítejte v programu losování.");
+            char volba = '0';
+            while (volba != '3')
+            { 
+                Console.WriteLine("1 - Losovat další číslo");
+                Console.WriteLine("2 - Vypsat čísla");
+                Console.WriteLine("3 - Konec");
+                volba = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                switch(volba)
+                {
+                    case '1':
+                        Console.WriteLine("Padlo číslo: {0}", losovac.Losuj());
+                        break;
+                    case '2':
+                        Console.WriteLine("Padla čísla: {0}", losovac.Vypis());
+                        break;
+                    case '3':
+                        Console.WriteLine("Děkuji za použití programu.");
+                        break;
+                    default:
+                        Console.WriteLine("Neplatná volba, zadejte prosím znovu");
+                        break;
+                }
+            }
+        }
+    }
+}
